374504185f91c88d687b5ca86f312eb04652976e
Zha0rong Li <zhaorong@bu.edu>
2021-02-17 12:35:50 -0500

    Update server_01.R

--- inst/shiny/server/server_01.R
+++ inst/shiny/server/server_01.R
@@ -11,6 +11,7 @@
 # Define server logic required to draw a histogram
     output$distPlot <- renderPlot({
 
+<<<<<<< Updated upstream
         # generate bins based on input$bins from ui.R
         x    <- faithful[, 2]
         bins <- seq(min(x), max(x), length.out = input$bins + 1)
@@ -19,3 +20,59 @@
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
 
     })
+=======
+### Ingest user data
+
+
+### Create batch design table when covariate selected
+
+
+    output$text <- renderText("this is a test")
+
+    observeEvent(input$covariate, {
+        req(se)
+        output$confoundingTable <- renderTable({
+            spcc <<- std_pearson_corr_coef(se, input$covariate)
+            cv <<- cramers_v(se, input$covariate)
+        })
+    })
+shinyServer(function(input, output, session) {
+ reactivevalue=reactiveValues(counts_location='',metadata_location='',Batch_Variable_Name='')
+source('observer.R')
+observe({
+    # Look for user file upload
+    if (!is.null(input$counts) & !is.null(input$md)){
+        se <<- ingest_data(input$counts$datapath, input$md$datapath)
+    }
+    else if (!is.null(input$se)){
+        ### THIS NEEDS TO BE TESTED
+        se <<- SummarizedExperiment(input$se$datapath)
+    }
+    else {
+        se <<- NULL
+    }
+    # Populate drop down menu with covariates
+    req(se)
+    cols <- names(colData(se))
+    covs <- cols[cols != 'Batch']
+    updateSelectInput(inputId = "covariate", choices = covs)
+})
+observeEvent(input$covariate, {
+    req(se)
+    output$summaryTable <- renderTable({
+        bd <<- batch_design(se, input$covariate)
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+}
+>>>>>>> Stashed changes
